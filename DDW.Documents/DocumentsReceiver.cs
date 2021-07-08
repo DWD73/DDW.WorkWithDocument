ff3a8ca957e3c1c0be884327f61f768ed675a7ff
Дмитрий <User@DEMIN-DV>
2021-07-08 17:54:05 +0300

    Добавьте файлы проекта.

---
+++ DDW.Documents/DocumentsReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDW.Documents
+{
+    public class DocumentsReceiver
+    {
+
+        
+
+    }
+
+    public class DocumentArgs : EventArgs
+    {
+
+    }
+}
