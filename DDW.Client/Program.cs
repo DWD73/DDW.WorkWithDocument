4b2343b2ae7ff55c946c130c673bc497dad0734b
Дмитрий <User@DEMIN-DV>
2021-07-09 17:50:53 +0300

    Итог 09.07.21

---
+++ DDW.Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using DDW.Document;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDW.Client
+{
+    class Program
+    {
+        const string pathDirectory = @"C:\Users\User\Desktop\TargetDirectory";
+        
+        static bool stopReceiveDocuments = false;
+
+        static void Main(string[] args)
+        {           
+            DocumentsReceiver documentsReceiver = new DocumentsReceiver();
+            documentsReceiver.DocumentsReady += DocumentsReceiver_DocumentsReady;
+            documentsReceiver.TimeOut += DocumentsReceiver_TimeOut;
+
+            documentsReceiver.Start(pathDirectory, 30000);
+
+            while(!stopReceiveDocuments)
+            {
+                
+            }
+
+            foreach (var doc in documentsReceiver.GetDocuments())
+                Console.WriteLine($"\t{doc}");
+
+            Console.ReadLine();
+            
+        }
+
+        private static void DocumentsReceiver_TimeOut(object sender, DocumentsArgs e)
+        {
+            
+            Console.WriteLine($"Время для приема документов вышло");
+            stopReceiveDocuments = true;
+        }
+
+        private static void DocumentsReceiver_DocumentsReady(object sender, DocumentsArgs e)
+        {
+            Console.WriteLine($"\nВсего загружено документов {e.CountFiles}\n");
+            stopReceiveDocuments = true;
+        }
+    }
+}
---
+++ DDW.Document/DocumentsReceiver.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DDW.Document
+{
+    public class DocumentsReceiver
+    {
+        FileSystemWatcher fileSystemWatcher;
+        List<string> documents;
+        //public IReadOnlyList<string> documentsRead { get; }
+
+
+        Timer timer;
+
+        public DocumentsReceiver()
+        {
+            documents = new List<string>();
+            
+            SetTimer();
+
+            fileSystemWatcher = new FileSystemWatcher
+            {               
+                NotifyFilter = NotifyFilters.FileName,
+                Filters = { "Паспорт.jpg", "Заявление.txt", "Фото.jpg" },
+                EnableRaisingEvents = false,
+                IncludeSubdirectories = false              
+            };
+
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+        }
+
+        private void SetTimer()
+        {          
+            timer = new Timer();          
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = false;           
+        }
+
+        public IReadOnlyList<string> GetDocuments()
+        {
+            return documents.AsReadOnly();
+        }
+
+        private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //DocumentsArgs args = new DocumentsArgs(e.Name);
+
+            if (!documents.Contains(e.Name) && documents.Count < 3)
+            {
+                documents.Add(e.Name);               
+            }
+
+            if(documents.Count == 3)
+            {
+                OnDocumentsReady(new DocumentsArgs(e.Name, documents.Count));
+            }
+
+            
+        }
+
+        public void Start(string path, double waitingInterval)
+        {
+            fileSystemWatcher.Path = path;
+            fileSystemWatcher.EnableRaisingEvents = true;
+
+            timer.Interval = waitingInterval;
+            timer.Start();           
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            fileSystemWatcher.EnableRaisingEvents = false;
+            OnTimeOut(new DocumentsArgs(null, documents.Count));
+        }
+
+
+        public event EventHandler<DocumentsArgs> DocumentsReady;
+        public event EventHandler<DocumentsArgs> TimeOut;
+
+        protected virtual void OnDocumentsReady(DocumentsArgs e)
+        {
+            DocumentsReady?.Invoke(this, e);
+        }
+
+        protected virtual void OnTimeOut(DocumentsArgs e)
+        {
+            TimeOut?.Invoke(this, e);
+        }
+    }
+
+    
+    public class DocumentsArgs : EventArgs
+    {
+        /// <summary>
+        /// Имя загружаемого документа
+        /// </summary>
+        public string FileName { get; }
+
+        public int CountFiles { get; }
+
+
+        public DocumentsArgs(string fileName, int countFiles)
+        {
+            FileName = string.IsNullOrEmpty(fileName) ? string.Empty : fileName;
+            CountFiles = countFiles;
+        }
+
+        
+    }
+}
